f0ea9b60cf4a774d694056f51965bf53ff1bc306
Mrunal Khade <83811490+mrunalk05@users.noreply.github.com>
2023-01-06 22:55:20 +0530

    ui added

---
+++ frontend2/frontend/frontend/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    return render(request, "index.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def card(request):
+    return render(request, "card.html")
